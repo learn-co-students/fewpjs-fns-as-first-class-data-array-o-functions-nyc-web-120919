e4096b08e91850943f2ea76b4dc3c15910204e90
github username <github email address>
2020-01-30 08:42:48 -0500

    Done.

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function unleashDog(dogName, dogBreed) {
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
- return routine.map(x => x(dogName, dogBreed));
+ return routine.map(func => func(dogName, dogBreed));
 }
